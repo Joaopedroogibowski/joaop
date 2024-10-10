04e662a6540ea1e2d3192fefa4ab75730c15c9c2
Joaopedroogibowski <163015795+Joaopedroogibowski@users.noreply.github.com>
2024-10-10 11:35:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual e a capital do acre?',
+    'Rio Branco'
+)
+
+criaCartao(
+    'futebol',
+    'Quem foi o camisa 10 em 2006 na selecao?',
+    'Ronaldinho Gaucho'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Academia',
+    'qual e o nome do objeto para fazer supino inclinado',
+    'halter'
+)
